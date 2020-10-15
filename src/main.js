dc5c53d9f3a010a048f631f7b0f9e4cfb1468a96
Nikita Shkarupa <n.shkarupa@yahoo.com>
2020-10-15 20:05:05 +0200

    add: vue router

--- src/main.js
+++ src/main.js
@@ -1,4 +1,23 @@
 import { createApp } from 'vue';
-import App from './App.vue';
+import { createRouter, createWebHistory } from 'vue-router';
 
-createApp(App).mount('#app');
+import App from './App.vue';
+import Home from './pages/home.vue';
+import Login from './pages/login.vue';
+import Registration from './pages/registration.vue';
+
+const router = createRouter({
+	history: createWebHistory(),
+	routes: [
+		{ path: '/', component: Home },
+		{ path: '/login', component: Login },
+		{ path: '/registration', component: Registration },
+	],
+});
+const app = createApp(App);
+
+app.use(router);
+
+router.isReady().then(() => {
+	app.mount('#app');
+});
---
+++ src/pages/home.vue
@@ -0,0 +1,3 @@
+<template>
+	<h1>Bon-voyage!</h1>
+</template>
