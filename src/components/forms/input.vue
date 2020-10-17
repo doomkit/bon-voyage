f480645b7eac71c419e353728505424ac4cc06d3
Nikita Shkarupa <n.shkarupa@yahoo.com>
2020-10-17 03:13:35 +0200

    add: icons, form autocomplete

--- src/components/forms/input.vue
+++ src/components/forms/input.vue
@@ -1,31 +1,61 @@
 <template>
-  <div class="relative flex flex-col w-full">
-    <label
-      :for="label"
-      class="mb-1 text-xs sm:text-sm tracking-wide text-gray-600 cursor-pointer"
-    >
-      {{ label }}
-    </label>
+	<div class="relative flex flex-col w-full">
+		<label
+			:for="label"
+			class="mb-1 text-xs sm:text-sm tracking-wide text-gray-600 cursor-pointer"
+		>
+			{{ label }}
+		</label>
 
-    <input
-      :id="label"
-      type="text"
-      :placeholder="placeholder"
-      class="text-sm sm:text-base text-white placeholder-gray-500 w-full rounded bg-gray-900 focus:bg-gray-700 focus:outline-none py-2 pr-2 pl-12"
-    />
-    <div
-      class="absolute text-gray-500 text-sm sm:text-base w-10 flex justify-center rounded-l bottom-0 p-2"
-    >
-      X
-    </div>
-  </div>
+		<input
+			:id="label"
+			:type="type"
+			:name="name"
+			:placeholder="placeholder"
+			class="text-sm sm:text-base text-white placeholder-gray-500 w-full rounded bg-gray-900 focus:bg-gray-700 focus:outline-none py-2 px-2"
+			:class="[iconClass && 'pl-12']"
+		/>
+		<div
+			v-if="iconClass"
+			class="absolute text-gray-500 text-sm sm:text-base w-10 flex justify-center rounded-l bottom-0 p-3"
+		>
+			<font-awesome-icon :icon="iconClass" />
+		</div>
+	</div>
 </template>
 
 <script>
-export default {
-  props: {
-    label: String,
-    placeholder: String,
-  },
-};
+	export default {
+		props: {
+			label: String,
+			placeholder: String,
+			iconClass: String,
+			name: String,
+			type: String,
+		},
+	};
 </script>
+
+<style scoped>
+	/* Change autocomplete styles in WebKit */
+	input:-webkit-autofill,
+	input:-webkit-autofill:hover,
+	textarea:-webkit-autofill,
+	textarea:-webkit-autofill:hover,
+	select:-webkit-autofill,
+	select:-webkit-autofill:hover {
+		border: none;
+		-webkit-text-fill-color: white;
+		-webkit-box-shadow: 0 0 0px 1000px #1a202c inset;
+		transition: background-color 5000s ease-in-out 0s;
+	}
+
+	input:-webkit-autofill:focus,
+	textarea:-webkit-autofill:focus,
+	select:-webkit-autofill:focus {
+		border: none;
+		-webkit-text-fill-color: white;
+		-webkit-box-shadow: 0 0 0px 1000px #4a5568 inset;
+		transition: background-color 5000s ease-in-out 0s;
+	}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
 import { createApp } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faUserCircle, faUnlockAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
 import App from './app.vue';
 import Dashboard from './pages/dashboard.vue';
 import Login from './pages/login.vue';
 import Registration from './pages/registration.vue';
 import Trips from './pages/trips.vue';
+
+library.add([faUserCircle, faUnlockAlt]);
 
 const router = createRouter({
 	history: createWebHistory(),
@@ -25,6 +31,7 @@ const router = createRouter({
 const app = createApp(App);
 
 app.use(router);
+app.component('font-awesome-icon', FontAwesomeIcon);
 
 router.isReady().then(() => {
 	app.mount('#app');
