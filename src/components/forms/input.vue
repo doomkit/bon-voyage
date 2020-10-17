754fff0ebfa71537b2d4c6899f17d6a91022ad02
Nikita Shkarupa <n.shkarupa@yahoo.com>
2020-10-17 02:01:34 +0200

    add: login form

---
+++ src/components/forms/input.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="relative flex flex-col w-full">
+    <label
+      :for="label"
+      class="mb-1 text-xs sm:text-sm tracking-wide text-gray-600 cursor-pointer"
+    >
+      {{ label }}
+    </label>
+
+    <input
+      :id="label"
+      type="text"
+      :placeholder="placeholder"
+      class="text-sm sm:text-base text-white placeholder-gray-500 w-full rounded bg-gray-900 focus:bg-gray-700 focus:outline-none py-2 pr-2 pl-12"
+    />
+    <div
+      class="absolute text-gray-500 text-sm sm:text-base w-10 flex justify-center rounded-l bottom-0 p-2"
+    >
+      X
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: String,
+    placeholder: String,
+  },
+};
+</script>
--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,9 +1,36 @@
 <template>
-	<h1>Login</h1>
+  <div
+    class="container mx-auto min-h-screen flex flex-col justify-center items-center py-4 px-2 sm:px-4"
+  >
+    <h1 class="text-gray-800 text-2xl text-center font-bold mb-2">
+      Sign in to your account
+    </h1>
+    <p class="text-gray-700 mb-6">
+      Not a member?
+      <router-link to="/registration" v-slot="{ href, navigate }">
+        <a
+          :href="href"
+          @click="navigate"
+          class="text-indigo-500 hover:text-indigo-700 focus:text-indigo-800"
+        >
+          Create account
+        </a>
+      </router-link>
+    </p>
+    <div
+      class="w-full md:w-3/4 lg:w-1/2 bg-gray-800 rounded-lg py-6 px-4 sm:px-6 shadow-2xl space-y-4"
+    >
+      <the-input label="Username" placeholder="Username"></the-input>
+      <the-input label="Password" placeholder="Password"></the-input>
+    </div>
+  </div>
 </template>
 
 <script>
-	export default {};
+import TheInput from "./../components/forms/input";
+
+export default {
+  components: { TheInput },
+};
 </script>
 
-<style></style>
