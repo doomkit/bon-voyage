efe10b5c780baf3ddc14f780eae5abd8349f2817
Nikita Shkarupa <n.shkarupa@yahoo.com>
2020-10-17 22:34:35 +0200

    update: login page

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,7 +1,8 @@
 <template>
 	<div
-		class="container mx-auto min-h-screen flex flex-col justify-center items-center py-4 px-2 sm:px-4"
+		class="container mx-auto min-h-screen flex flex-col justify-center items-center py-4 px-2 sm:px-4 pb-16"
 	>
+		<img class="mx-auto h-12 w-auto" src="@/assets/logo.svg" />
 		<h1 class="text-gray-800 text-2xl text-center font-bold mb-2">
 			Sign in to your account
 		</h1>
@@ -18,30 +19,65 @@
 			</router-link>
 		</p>
 		<div
-			class="w-full md:w-3/4 lg:w-1/2 bg-gray-800 rounded-lg py-6 px-4 sm:px-6 shadow-2xl space-y-4"
+			class="w-full md:w-3/4 lg:w-1/2 bg-gray-800 rounded-lg py-6 px-4 sm:px-6 shadow-2xl"
 		>
-			<the-input
-				label="Username"
-				placeholder="Username"
-				iconClass="user-circle"
-				name="username"
-				type="text"
-			></the-input>
-			<the-input
-				label="Password"
-				placeholder="Password"
-				iconClass="unlock-alt"
-				name="password"
-				type="password"
-			></the-input>
+			<form @submit.prevent="onSubmit" class="space-y-2">
+				<the-input
+					label="Username"
+					placeholder="Username"
+					iconClass="user-circle"
+					name="username"
+					type="text"
+					autocomplete="off"
+					v-model="username"
+				></the-input>
+				<the-input
+					label="Password"
+					placeholder="Password"
+					iconClass="unlock-alt"
+					name="password"
+					type="password"
+					autocomplete="new-password"
+					v-model="password"
+				></the-input>
+
+				<div class="pt-6">
+					<the-button
+						text="Sign in"
+						type="submit"
+						iconClass="sign-in-alt"
+					></the-button>
+				</div>
+			</form>
 		</div>
 	</div>
 </template>
 
 <script>
+	import { ref } from 'vue';
+	import { useRouter } from 'vue-router';
+
 	import TheInput from './../components/forms/input';
+	import TheButton from './../components/ui/button';
 
 	export default {
-		components: { TheInput },
+		components: { TheInput, TheButton },
+		setup() {
+			const router = useRouter();
+
+			let username = ref('');
+			let password = ref('');
+
+			function onSubmit() {
+				console.log(`${username.value}:${password.value}`);
+				router.push('/dashboard');
+			}
+
+			return {
+				username,
+				password,
+				onSubmit,
+			};
+		},
 	};
 </script>
