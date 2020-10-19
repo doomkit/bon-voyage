df4e73d14a7858b9a185440d34ddbed601035651
Nikita Shkarupa <n.shkarupa@yahoo.com>
2020-10-19 14:32:21 +0200

    fix: navbar navigation behaviour

--- src/components/navigation/navigation-link.vue
+++ src/components/navigation/navigation-link.vue
@@ -1,11 +1,11 @@
 <template>
-	<router-link :to="path" v-slot="{ href, navigate, isActive }">
+	<router-link :to="path" v-slot="{ href, navigate, isExactActive }">
 		<a
 			:href="href"
 			@click="navigate"
 			:class="[
-				isActive && 'text-white bg-gray-900',
-				!isActive && 'text-gray-300 hover:bg-gray-700',
+				isExactActive && 'text-white bg-gray-900',
+				!isExactActive && 'text-gray-300 hover:bg-gray-700',
 			]"
 			class="text-base block px-3 py-2 rounded-md font-medium hover:text-white focus:outline-none focus:bg-gray-700"
 		>
--- src/components/ui/dropdown.vue
+++ src/components/ui/dropdown.vue
@@ -16,7 +16,12 @@
 					>
 						<a
 							:href="href"
-							@click="navigate"
+							@click="
+								() => {
+									$emit('update:show', false);
+									navigate;
+								}
+							"
 							:class="[isActive && 'font-bold bg-gray-100']"
 							class="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out"
 						>
