8fc4d55ed7d10c875de54102afa79a88ebab621b
Nikita Shkarupa <n.shkarupa@yahoo.com>
2020-10-19 14:33:32 +0200

    add: dashboard router animation

--- src/pages/dashboard.vue
+++ src/pages/dashboard.vue
@@ -1,7 +1,12 @@
 <template>
 	<div class="w-full h-full">
 		<navigation></navigation>
-		<router-view></router-view>
+
+		<router-view v-slot="{ Component }">
+			<transition name="dashboard">
+				<component :is="Component" />
+			</transition>
+		</router-view>
 	</div>
 </template>
 
@@ -12,3 +17,25 @@
 		components: { Navigation },
 	};
 </script>
+
+<style>
+	/* Router animation */
+	.dashboard-enter-from {
+		opacity: 0;
+		transform: translateY(40px) scale(0.95);
+	}
+	.dashboard-leave-to {
+		opacity: 0;
+		transform: translateY(40px) scale(0.95);
+	}
+	.dashboard-enter-active {
+		transition: all 0.3s ease 0.3s;
+	}
+	.dashboard-leave-active {
+		transition: all 0.3s ease;
+	}
+	.dashboard-enter-to .dashboard-leave-from {
+		opacity: 1;
+		transform: translateY(0) scale(1);
+	}
+</style>
