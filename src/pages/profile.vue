e840560ee45638b91df92ffbfacba068ef14a648
Nikita Shkarupa <n.shkarupa@yahoo.com>
2020-10-23 22:31:31 +0200

    add: profile main details section

--- src/pages/profile.vue
+++ src/pages/profile.vue
@@ -1,108 +1,38 @@
 <template>
-	<p>
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam luctus odio
-		quam, ac cursus ligula hendrerit sed. Sed dignissim est felis, a vulputate
-		orci egestas nec. In porta, ex vitae lacinia gravida, quam purus interdum
-		velit, pharetra egestas nisi dui eu augue. Pellentesque vel sodales lectus.
-		Nullam mattis dolor magna, ac cursus arcu mollis sit amet. Vivamus faucibus
-		commodo ipsum nec maximus. Nam blandit vehicula massa. Duis facilisis
-		lobortis enim nec facilisis. Suspendisse scelerisque mi tempor ex
-		consectetur tincidunt. Proin enim dui, vehicula non convallis ac, vulputate
-		vitae metus. Suspendisse sed massa sit amet augue ultrices porttitor. Etiam
-		dignissim sem eget nisl rhoncus ornare. Mauris imperdiet, elit ut dapibus
-		congue, quam orci lacinia sem, a sollicitudin ipsum ipsum ac lectus. Proin
-		vel ligula eu ipsum eleifend finibus. Maecenas semper vehicula urna, sed
-		mollis enim imperdiet at. Maecenas at sollicitudin sapien. Vivamus at
-		facilisis neque. Nam volutpat dignissim felis ac malesuada. Suspendisse
-		nulla mauris, congue non consectetur ut, congue sed tellus. Nulla at
-		suscipit ipsum, in dictum purus. Aliquam viverra condimentum finibus. Proin
-		faucibus egestas ligula sagittis interdum. Suspendisse libero lectus, ornare
-		vitae turpis non, fringilla molestie tellus. Nam non ex odio. Maecenas
-		auctor odio turpis, at dapibus velit tincidunt vitae. Vivamus condimentum
-		vulputate efficitur. Quisque sit amet pulvinar nunc. In hac habitasse platea
-		dictumst. Cras placerat cursus posuere. Fusce eget faucibus lacus. Phasellus
-		eget fermentum lacus, sed accumsan arcu. Quisque luctus, mauris a dictum
-		placerat, velit ipsum pellentesque felis, non rutrum eros ex nec nibh. Ut ut
-		hendrerit arcu. Phasellus suscipit urna sit amet suscipit accumsan. Maecenas
-		tincidunt hendrerit nunc fermentum vestibulum. Class aptent taciti sociosqu
-		ad litora torquent per conubia nostra, per inceptos himenaeos. Suspendisse
-		ac dignissim neque. Curabitur sit amet felis felis. Praesent eget nisl ante.
-		Donec ornare efficitur tellus in finibus. In blandit varius quam ac dapibus.
-		Morbi pulvinar orci in justo interdum, et imperdiet arcu pharetra. Etiam
-		tristique dictum risus non venenatis. Praesent consectetur lectus diam, at
-		venenatis neque pretium sed. Phasellus vehicula aliquam congue. Praesent vel
-		ex nec erat faucibus imperdiet ac eu massa. Donec et ullamcorper velit.
-		Suspendisse luctus odio sit amet efficitur porttitor. Ut vitae fermentum
-		lacus, id rhoncus massa. Phasellus interdum massa nec risus pulvinar, non
-		egestas est lacinia. Proin consectetur quam non lobortis tempus. Mauris
-		ultricies ultrices justo non consequat. Sed ac ultrices lacus. Nulla
-		facilisi. Nunc fringilla eros felis, at volutpat mauris accumsan et. Aenean
-		nec luctus tortor. Morbi scelerisque turpis in nulla elementum, sed tempus
-		urna ultricies. Proin sit amet leo nec est lobortis sagittis ut eu mi.
-		Nullam rutrum libero nec velit ornare pretium. Fusce lobortis lectus
-		tristique, porta tellus ac, fermentum turpis. Nullam cursus leo ligula, sed
-		auctor nulla sagittis quis. Nulla mi nunc, malesuada rhoncus ipsum id, porta
-		volutpat neque. Ut rutrum interdum orci, ac porttitor arcu posuere non.
-		Integer dapibus consectetur sapien et mollis. Curabitur scelerisque
-		condimentum ultricies. Pellentesque sit amet dignissim magna. Duis in est
-		eget velit fermentum sodales non vel nisi. Quisque gravida scelerisque eros,
-		et ornare magna efficitur vitae. Vivamus nulla neque, porta eget blandit et,
-		mollis ut dui. Morbi pellentesque mattis mauris, id interdum ligula
-		consectetur sed. Nam sed orci luctus, convallis neque sit amet, tristique
-		tortor. Cras lacus mauris, maximus quis rutrum eget, feugiat non velit. Sed
-		imperdiet, tortor et tincidunt pharetra, magna lectus porta ante, volutpat
-		consectetur nisi nisi vitae ante. Curabitur ac maximus dui. Nullam non
-		tortor augue. Nullam pellentesque dui vel nisi tristique, at finibus nunc
-		consequat. Cras vehicula ac felis porttitor elementum. Fusce sed hendrerit
-		nisi. Morbi nec ipsum eu nisl lobortis convallis. Morbi lacinia velit in
-		metus dapibus suscipit. Vestibulum ut metus et metus varius scelerisque ac
-		nec enim. Fusce dapibus tempus pellentesque. Fusce ac volutpat nibh. Aliquam
-		id blandit sem. Phasellus posuere tincidunt purus, eu sagittis lorem maximus
-		vitae. Nam vel vehicula purus. Morbi malesuada, felis tempus vestibulum
-		viverra, quam est venenatis mauris, sed aliquam dolor libero sit amet enim.
-		Mauris sit amet risus fermentum, tristique lorem eget, egestas neque. Aenean
-		urna turpis, lobortis tempus tortor et, porta rhoncus lacus. Nunc varius
-		elementum ultrices. Nunc molestie nunc felis, nec rutrum odio consequat in.
-		Etiam justo ante, dignissim nec ante ac, tempor imperdiet quam. Aliquam
-		fermentum dolor vel ullamcorper pellentesque. Sed ac finibus nisi. Nam
-		gravida ultricies placerat. Sed nec sodales tortor. Sed dignissim
-		scelerisque tortor, a semper arcu viverra semper. Donec vel risus
-		ullamcorper, porta dui a, mollis diam. Nullam fermentum tortor ut dolor
-		sodales, nec suscipit dolor ultrices. Sed sit amet augue in nisi semper
-		placerat vitae hendrerit diam. In facilisis elit id leo pretium, et
-		hendrerit felis congue. Etiam accumsan eget tellus vitae euismod.
-		Pellentesque eu turpis vitae diam fermentum aliquam. Praesent non tempor
-		justo. Proin tincidunt lorem nunc, ac iaculis libero pellentesque non. Donec
-		et nunc risus. Maecenas pellentesque sagittis dui vel euismod. Sed consequat
-		vulputate cursus. Proin justo libero, finibus vitae libero iaculis,
-		condimentum varius mauris. Praesent sapien lorem, aliquet nec pretium sed,
-		euismod non orci. Vivamus pretium mi risus, vel porttitor quam ullamcorper
-		eleifend. Morbi mattis, tortor a lobortis lacinia, nibh est vulputate ipsum,
-		nec elementum velit ex et risus. Aliquam ornare ex quis est eleifend
-		elementum. Etiam tempor, elit at sollicitudin consequat, nulla turpis
-		accumsan nibh, id ultricies nisl risus vel tortor. Cras quis sapien non
-		dolor imperdiet placerat. Morbi gravida feugiat urna sit amet finibus.
-		Curabitur nisi justo, cursus ut enim ac, aliquam condimentum ante. Donec
-		sagittis auctor neque, sed bibendum erat pretium nec. Aenean laoreet, enim
-		vitae tempor congue, arcu nisl blandit mi, id finibus augue velit ut mi.
-		Fusce tincidunt dolor et libero pretium, porta tristique tortor sagittis.
-		Morbi magna diam, placerat lobortis sodales eu, venenatis et lacus. Donec
-		non massa a nibh vehicula consequat. In hac habitasse platea dictumst. Sed
-		vulputate placerat nibh, eget sagittis arcu facilisis nec. Donec nec blandit
-		odio. Curabitur ut ante magna. Praesent bibendum sapien consectetur urna
-		auctor condimentum. Phasellus eu viverra purus, cursus congue augue. Nunc
-		vel nibh sapien. Vivamus quis varius nibh. Donec augue libero, luctus ut
-		libero et, accumsan rhoncus risus. Nam volutpat elit at erat condimentum
-		eleifend. Ut quis leo vel magna porta sollicitudin. Vivamus ultricies
-		lobortis placerat. Suspendisse tempus sit amet turpis cursus bibendum.
-		Integer magna lorem, euismod non sem sit amet, aliquet fringilla lacus.
-		Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
-		inceptos himenaeos. Vestibulum sit amet viverra augue. Proin pharetra libero
-		et ipsum sagittis pharetra. Donec gravida sapien eget pulvinar scelerisque.
-		Ut maximus tellus neque, a mollis eros pellentesque ac. Praesent condimentum
-		tortor diam, aliquam posuere dolor condimentum id. In massa neque, porta at
-		nunc sed, tristique ultrices purus. Pellentesque interdum tristique diam.
-		Morbi nec commodo ante, nec dictum libero. Fusce pharetra facilisis massa ac
-		tempus. Ut a sagittis velit, id venenatis ante.
-	</p>
+  <div class="w-full mx-auto flex space-x-8">
+    <div
+      class="w-1/3 p-4 rounded shadow-lg bg-white flex flex-col items-center"
+    >
+      <div
+        class="w-24 h-24 bg-gray-400 rounded-lg mb-4 text-white text-6xl flex justify-center items-center"
+      >
+        <font-awesome-icon icon="smile" />
+      </div>
+      <h1 class="font-bold">Name Surname</h1>
+      <h2 class="text-sm text-gray-600 mb-4">@username</h2>
+      <div
+        class="flex justify-between items-center divide-x divide-gray-300 text-center mb-6"
+      >
+        <div class="px-2">
+          <p class="text-gray-500 text-xs mb-2">Trips planned</p>
+          <p class="text-indigo-500 text-sm font-bold">2</p>
+        </div>
+        <div class="px-2">
+          <p class="text-gray-500 text-xs mb-2">Miles travelled</p>
+          <p class="text-indigo-500 text-sm font-bold">13.5</p>
+        </div>
+        <div class="px-2">
+          <p class="text-gray-500 text-xs mb-2">Places visited</p>
+          <p class="text-indigo-500 text-sm font-bold">7</p>
+        </div>
+      </div>
+      <the-button
+        :iconClass="'sign-out-alt'"
+        :type="'button'"
+        :text="'Logout'"
+      ></the-button>
+    </div>
+
+    <div class="w-2/3 p-4 rounded shadow-lg bg-white"></div>
+  </div>
 </template>
