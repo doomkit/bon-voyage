d53eb0dfd9bc6f03a31aaab559083ed0ba7f86e1
Nikita Shkarupa <n.shkarupa@yahoo.com>
2020-10-26 23:11:23 +0100

    update: button

--- src/components/ui/button.vue
+++ src/components/ui/button.vue
@@ -1,12 +1,17 @@
 <template>
 	<button
 		:type="type"
-		class="group relative w-full flex justify-center py-2 px-4 text-sm leading-6 font-medium rounded text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out"
+		class="group relative w-full flex justify-center py-2 px-4 text-sm leading-6 font-medium rounded text-white focus:outline-none  focus:shadow-outline-indigo transition duration-150 ease-in-out"
+		:class="{
+			'bg-indigo-600 hover:bg-indigo-500 focus:border-indigo-700 active:bg-indigo-700': !color || color === 'primary',
+			'bg-red-600 hover:bg-red-500 focus:border-red-700 active:bg-red-700': color === 'danger',
+			'bg-gray-600 hover:bg-gray-500 focus:border-gray-700 active:bg-gray-700': color === 'secondary'
+		}"
 	>
 		<span
 			class="text-indigo-400 absolute left-0 inset-y-0 flex items-center pl-3"
 		>
-			<font-awesome-icon :icon="iconClass" />
+			<font-awesome-icon v-if="iconClass" :icon="iconClass" />
 		</span>
 		{{ text }}
 	</button>
@@ -18,6 +23,7 @@
 			iconClass: String,
 			type: String,
 			text: String,
+			color: String		// primary, secondary, danger
 		},
 	};
 </script>
